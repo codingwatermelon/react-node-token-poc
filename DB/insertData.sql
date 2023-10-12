9e5a0f46fa42c90d2a84087a0bd1287ca2f20e00
codingwatermelon <32379178+codingwatermelon@users.noreply.github.com>
2023-10-12 02:05:08 -0700

    add semicolons

--- DB/insertData.sql
+++ DB/insertData.sql
@@ -10,8 +10,8 @@ insert into PropertiesDetails (image_path, base_value, purchase_date) VALUES ('h
 insert into PropertiesDetails (image_path, base_value, purchase_date) VALUES ('https://photos.zillowstatic.com/fp/e5c45aa3886e9b832d5a5012b25fc0ed-uncropped_scaled_within_1536_1152.webp', 400, '2006-11-12');
 insert into PropertiesDetails (image_path, base_value, purchase_date) VALUES ('https://photos.zillowstatic.com/fp/510de7bae4112e920d4e688eaac98b12-uncropped_scaled_within_1536_1152.webp', 350, '2009-07-20');
 
-insert into PropertiesMaintenance (property_id, maintenance_name, cost, maintenance_type, due_date) VALUES (1, 'AC replacement', 200, 'preventative', '2023-10-30')
-insert into PropertiesMaintenance (property_id, maintenance_name, cost, maintenance_type, due_date) VALUES (1, 'Roof replacement', 10000, 'emergency', '2023-10-30')
-insert into PropertiesMaintenance (property_id, maintenance_name, cost, maintenance_type, due_date) VALUES (3, 'HVAC inspection', 100, 'preventative', '2023-11-30')
-insert into PropertiesMaintenance (property_id, maintenance_name, cost, maintenance_type, due_date) VALUES (4, 'Siding installation', 1000, 'cosmetic', '2023-12-31')
-insert into PropertiesMaintenance (property_id, maintenance_name, cost, maintenance_type, due_date) VALUES (5, 'Annual septic pump', 250, 'scheduled', '2023-11-30')
+insert into PropertiesMaintenance (property_id, maintenance_name, cost, maintenance_type, due_date) VALUES (1, 'AC replacement', 200, 'preventative', '2023-10-30');
+insert into PropertiesMaintenance (property_id, maintenance_name, cost, maintenance_type, due_date) VALUES (1, 'Roof replacement', 10000, 'emergency', '2023-10-30');
+insert into PropertiesMaintenance (property_id, maintenance_name, cost, maintenance_type, due_date) VALUES (3, 'HVAC inspection', 100, 'preventative', '2023-11-30');
+insert into PropertiesMaintenance (property_id, maintenance_name, cost, maintenance_type, due_date) VALUES (4, 'Siding installation', 1000, 'cosmetic', '2023-12-31');
+insert into PropertiesMaintenance (property_id, maintenance_name, cost, maintenance_type, due_date) VALUES (5, 'Annual septic pump', 250, 'scheduled', '2023-11-30');
