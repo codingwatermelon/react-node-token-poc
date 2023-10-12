e38edcee90d44cec5e801f1cd4b922bf72b30e7e
codingwatermelon <32379178+codingwatermelon@users.noreply.github.com>
2023-10-12 02:04:26 -0700

    add another table

--- DB/createTables.sql
+++ DB/createTables.sql
@@ -7,7 +7,7 @@ CREATE TABLE IF NOT EXISTS Properties (
    property_description VARCHAR ( 200 ) NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS PropertyDetails (
+CREATE TABLE IF NOT EXISTS PropertiesDetails (
    id int GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
    image_path VARCHAR ( 200 ) NOT NULL,
    base_value INT NOT NULL,
@@ -18,3 +18,15 @@ CREATE TABLE IF NOT EXISTS PropertyDetails (
 	    ON DELETE CASCADE
 );
 
+CREATE TABLE IF NOT EXISTS PropertiesMaintenance (
+   id int GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+   property_id INT NOT NULL,
+   maintenance_name VARCHAR ( 200 ) NOT NULL,
+   cost INT NOT NULL,
+   maintenance_type VARCHAR ( 200 ) NOT NULL,
+   due_date DATE NOT NULL,
+   CONSTRAINT fk_property_id
+       FOREIGN KEY(property_id) 
+	    REFERENCES Properties(id)
+	    ON DELETE CASCADE
+);
--- DB/resetTables.sql
+++ DB/resetTables.sql
@@ -1,2 +1,4 @@
 DROP TABLE IF EXISTS propertydetails; 
+DROP TABLE IF EXISTS propertiesdetails; 
 DROP TABLE IF EXISTS properties;
+DROP TABLE IF EXISTS propertiesmaintenance;
