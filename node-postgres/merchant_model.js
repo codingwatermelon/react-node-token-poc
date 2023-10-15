97ba3fcd838edd0055709175c22ff4d4e85136e5
codingwatermelon <jtorres7797@gmail.com>
2023-10-15 11:44:09 +0000

    Add example express app

---
+++ node-postgres/merchant_model.js
@@ -0,0 +1,50 @@
+const Pool = require('pg').Pool
+const pool = new Pool({
+  user: 'postgres',
+  host: '192.168.64.2',
+  database: 'test_merchant_db',
+  password: 'Watermelon!123!',
+  port: 5432,
+});
+
+const getMerchants = () => {
+  return new Promise(function(resolve, reject) {
+    pool.query('SELECT * FROM merchants ORDER BY id ASC', (error, results) => {
+      if (error) {
+        reject(error)
+      }
+      resolve(results.rows);
+    })
+  })
+}
+const createMerchant = (body) => {
+  return new Promise(function(resolve, reject) {
+    const { name, email } = body
+    pool.query('INSERT INTO merchants (name, email) VALUES ($1, $2) RETURNING *', [name, email], (error, results) => {
+      if (error) {
+        reject(error)
+      }
+      resolve(`A new merchant has been added added: ${results.rows[0]}`)
+    })
+  })
+}
+const deleteMerchant = () => {
+  return new Promise(function(resolve, reject) {
+    const id = parseInt(request.params.id)
+    console.log(id)
+    console.log("test")
+    pool.query('DELETE FROM merchants WHERE id = $1', [id], (error, results) => {
+      if (error) {
+	console.log(error)
+        reject(error)
+      }
+      resolve(`Merchant deleted with ID: ${id}`)
+    })
+  })
+}
+
+module.exports = {
+  getMerchants,
+  createMerchant,
+  deleteMerchant,
+}
