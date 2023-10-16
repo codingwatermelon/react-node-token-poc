887ebcc8d2df4c3fd2559b3376cb8efd5e36c13a
codingwatermelon <32379178+codingwatermelon@users.noreply.github.com>
2023-10-15 18:21:04 -0700

    Update index.js

--- node-postgres/index.js
+++ node-postgres/index.js
@@ -6,7 +6,7 @@ const merchant_model = require('./merchant_model')
 
 app.use(express.json())
 app.use(function (req, res, next) {
-  res.setHeader('Access-Control-Allow-Origin', 'http://192.168.64.3:3000');
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost');
   res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Access-Control-Allow-Headers, Origin, Accept, X-Requested-With');
   next();
