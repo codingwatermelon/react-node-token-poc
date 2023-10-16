56f7f1910bdecbabea8ba1ad8dc6dc74b4a7c9dc
codingwatermelon <32379178+codingwatermelon@users.noreply.github.com>
2023-10-15 18:51:23 -0700

    Update index.js

--- node-postgres/index.js
+++ node-postgres/index.js
@@ -5,7 +5,19 @@ const port = 3001
 const merchant_model = require('./merchant_model')
 
 const cors = require('cors');
-app.use(cors());
+const whitelist = ['http://localhost:3000'];
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true
+};
+app.use(cors(corsOptions));
+
 
 app.use(express.json())
 app.use(function (req, res, next) {
