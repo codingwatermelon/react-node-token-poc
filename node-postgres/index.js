3674c5c0aeac56f9e6ea95a9f603dcafdba2aa69
codingwatermelon <32379178+codingwatermelon@users.noreply.github.com>
2023-10-15 18:17:55 -0700

    update CORS headers

--- node-postgres/index.js
+++ node-postgres/index.js
@@ -8,7 +8,7 @@ app.use(express.json())
 app.use(function (req, res, next) {
   res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
   res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Access-Control-Allow-Headers');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Access-Control-Allow-Headers, Origin, Accept, X-Requested-With');
   next();
 });
 
