d2a4a58409cd6f097989b0edaccebee087f1aeaa
codingwatermelon <32379178+codingwatermelon@users.noreply.github.com>
2023-10-16 01:50:30 -0700

    Update index.js

--- node-postgres/index.js
+++ node-postgres/index.js
@@ -6,6 +6,7 @@ const merchant_model = require('./merchant_model')
 
 app.use(express.json())
 app.use(function (req, res, next) {
+  // TODO might have to restrict this
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Access-Control-Allow-Headers, Origin, Accept, X-Requested-With');
