a9a43b8d90e744ef41b4b3c0032cdb67044fc590
codingwatermelon <32379178+codingwatermelon@users.noreply.github.com>
2023-11-10 13:45:17 -0800

    Update Maintenance.jsx

--- JoeysHomesApp/pages/Maintenance/Maintenance.jsx
+++ JoeysHomesApp/pages/Maintenance/Maintenance.jsx
@@ -46,7 +46,13 @@ export default function Maintenance() {
                         type: typeFilter
                     }}
                 >
-                    <Box sx={{ p: 2.25 }}>
+                    <Box sx={{ 
+                        p: 2.25, 
+                        bgcolor: 'background.paper',
+                        boxShadow: 1,
+                        borderRadius: 2,
+                        p: 2,
+                        minWidth: 300,}}>
                     <div className="maintenance-info">
                         <h2>{format(new Date(task.due_date_epoch * 1000), "MMMM do, yyyy")}</h2>
                         <h3>{task.maintenance_name}</h3>
