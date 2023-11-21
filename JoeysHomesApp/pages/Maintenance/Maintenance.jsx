9868ce686b81784e59e2a230a3b67367f55544d4
codingwatermelon <32379178+codingwatermelon@users.noreply.github.com>
2023-11-21 05:40:07 -0800

    try putting function in different file

--- JoeysHomesApp/pages/Maintenance/Maintenance.jsx
+++ JoeysHomesApp/pages/Maintenance/Maintenance.jsx
@@ -16,7 +16,7 @@ import { Avatar, Box, Grid, Menu, MenuItem, Typography } from '@mui/material';
 import { useState } from "react";
 // search https://github.com/Saleh-Mubashar/React-Search/blob/master/App.js
 import TextField from "@mui/material/TextField";
-import List from "./List";
+import renderMaintenanceElements from "./test";
 
 // sx prop doc https://mui.com/system/getting-started/the-sx-prop/
 // box doc https://mui.com/material-ui/react-box/
@@ -60,73 +60,6 @@ export default function Maintenance() {
             }
             return prevParams
         })
-    }
-
-    function renderMaintenanceElements(props) {
-        const displayedMaintenanceTasks = props.input 
-            ? props.maintenance.filter(task => task.maintenance_name.startsWith(props.input))
-            : props.maintenance
-
-        const maintenanceElements = displayedMaintenanceTasks.map(task => (
-            <div key={task.id}>
-                <Link
-                    to={task.id}
-                    state={{
-                        search: `?${searchParams.toString()}`,
-                        type: typeFilter
-                    }}
-                >
-                    <Box sx={boxSX}>
-                        <div>
-                            <h2>{format(new Date(task.due_date_epoch * 1000), "MMMM do, yyyy")}</h2>
-                            <h3>{task.maintenance_name}</h3>
-                            <h3>{task.maintenance_type}</h3>
-                            <p><span>$</span>{task.cost}</p>
-                        </div>
-                    </Box>
-                
-                </Link>
-            </div>
-        ))
-        
-        return (
-            <>
-                <div className="van-list-filter-buttons">
-                    <button
-                        onClick={() => handleFilterChange("type", "simple")}
-                        className={
-                            `van-type simple 
-                        ${typeFilter === "simple" ? "selected" : ""}`
-                        }
-                    >Simple</button>
-                    <button
-                        onClick={() => handleFilterChange("type", "luxury")}
-                        className={
-                            `van-type luxury 
-                        ${typeFilter === "luxury" ? "selected" : ""}`
-                        }
-                    >Luxury</button>
-                    <button
-                        onClick={() => handleFilterChange("type", "rugged")}
-                        className={
-                            `van-type rugged 
-                        ${typeFilter === "rugged" ? "selected" : ""}`
-                        }
-                    >Rugged</button>
-
-                    {typeFilter ? (
-                        <button
-                            onClick={() => handleFilterChange("type", null)}
-                            className="van-type clear-filters"
-                        >Clear filter</button>
-                    ) : null}
-                </div>
-
-                <div className="maintenance-list">
-                    {maintenanceElements}
-                </div>
-            </>
-        )
     }
 
     // TODO Conditionally render certain tasks when search bar is being used
