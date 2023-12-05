b99b7703f7c923ef617bc3324b3de499987cfc79
codingwatermelon <32379178+codingwatermelon@users.noreply.github.com>
2023-12-05 13:11:57 -0800

    Update utils.js

--- JoeysHomesApp/utils.js
+++ JoeysHomesApp/utils.js
@@ -4,10 +4,14 @@ export async function requireAuth(request) {
     const pathname = new URL(request.url).pathname
     const isLoggedIn = localStorage.getItem("loggedin")
 
+    console.log("isLoggedIn")
+    console.log(isLoggedIn)
+
     if (!isLoggedIn) {
         console.log("not logged in, pathname")
         console.log(pathname)
         // TODO Figure out why this isn't working
+        //throw
         return redirect(
             `/login?message=You must log in first.&redirectTo=${pathname}`
         )
