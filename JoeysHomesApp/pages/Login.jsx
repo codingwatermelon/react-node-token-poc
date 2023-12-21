1d8f7807cde3075344ff573342054a52bc515220
codingwatermelon <32379178+codingwatermelon@users.noreply.github.com>
2023-12-21 02:55:42 -0800

    Update Login.jsx

--- JoeysHomesApp/pages/Login.jsx
+++ JoeysHomesApp/pages/Login.jsx
@@ -46,37 +46,38 @@ export default function Login() {
     if (isLoggedIn) {
         return redirect("account")
     }
+    else {
+        return (
+            <div className="login-container">
+                <h1>Sign in to your account</h1>
+                {message && <h3 className="red">{message}</h3>}
+                {errorMessage && <h3 className="red">{errorMessage}</h3>}
 
-    return (
-        <div className="login-container">
-            <h1>Sign in to your account</h1>
-            {message && <h3 className="red">{message}</h3>}
-            {errorMessage && <h3 className="red">{errorMessage}</h3>}
-
-            <Form 
-                method="post" 
-                className="login-form" 
-                replace
-            >
-                <input
-                    name="email"
-                    type="email"
-                    placeholder="Email address"
-                />
-                <input
-                    name="password"
-                    type="password"
-                    placeholder="Password"
-                />
-                <button
-                    disabled={navigation.state === "submitting"}
+                <Form 
+                    method="post" 
+                    className="login-form" 
+                    replace
                 >
-                    {navigation.state === "submitting"
-                        ? "Logging in..."
-                        : "Log in"
-                    }
-                </button>
-            </Form>
-        </div>   
-    )
+                    <input
+                        name="email"
+                        type="email"
+                        placeholder="Email address"
+                    />
+                    <input
+                        name="password"
+                        type="password"
+                        placeholder="Password"
+                    />
+                    <button
+                        disabled={navigation.state === "submitting"}
+                    >
+                        {navigation.state === "submitting"
+                            ? "Logging in..."
+                            : "Log in"
+                        }
+                    </button>
+                </Form>
+            </div>   
+        )
+    }
 }
