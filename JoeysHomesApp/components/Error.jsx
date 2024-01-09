d074b3505a2661dcb37f241b835da3260979c545
codingwatermelon <32379178+codingwatermelon@users.noreply.github.com>
2024-01-09 04:27:04 -0800

    Update Error.jsx

--- JoeysHomesApp/components/Error.jsx
+++ JoeysHomesApp/components/Error.jsx
@@ -1,29 +1,32 @@
 import React from "react"
-import { useRouteError } from "react-router-dom"
+import { useRouteError, isRouteErrorResponse } from "react-router-dom"
 
 export default function Error() {
     const error = useRouteError()
     
-    if (error.status == 403) {
-        return (
-            <>
-            <h1>Your session has timed out.</h1>
-            <Link
-                to="/login"
-            >
-                <h2>Click here to log in again</h2>
-            </Link>
-            </>
-        )
-    }
-    else {
-        // TODO If error is 403 Forbidden, then route user back to Login and mention that their session has timed out
-        console.log(error)
+    console.log(error)
+
+    if (isRouteErrorResponse(error)) {
         return (
             <>
             <h1>Error: {error.message}</h1>
             <pre>{error.status} - {error.statusText}</pre>
             </>
         )
+    }
+    else {
+        // If error is 403 Forbidden, then route user back to Login and mention that their session has timed out
+        if (error.response.status == 403) {
+            return (
+                <>
+                <h1>Your session has timed out.</h1>
+                <Link
+                    to="/login"
+                >
+                    <h2>Click here to log in again</h2>
+                </Link>
+                </>
+            )
+        }
     }
 }
