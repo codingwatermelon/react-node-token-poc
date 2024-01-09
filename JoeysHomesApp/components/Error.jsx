1726a9b052f2117396700453dc1b7d40fefd4494
codingwatermelon <32379178+codingwatermelon@users.noreply.github.com>
2024-01-09 04:12:47 -0800

    add login prompt if session has expired

--- JoeysHomesApp/components/Error.jsx
+++ JoeysHomesApp/components/Error.jsx
@@ -4,10 +4,25 @@ import { useRouteError } from "react-router-dom"
 export default function Error() {
     const error = useRouteError()
     
-    return (
-        <>
-        <h1>Error: {error.message}</h1>
-        <pre>{error.status} - {error.statusText}</pre>
-        </>
-    )
+    if (error.status == 403) {
+        return (
+            <>
+            <h1>Your session has timed out.</h1>
+            <Link
+                to="/login"
+            >
+                <h2>Click here to log in again</h2>
+            </Link>
+            </>
+        )
+    }
+    else {
+        // TODO If error is 403 Forbidden, then route user back to Login and mention that their session has timed out
+        return (
+            <>
+            <h1>Error: {error.message}</h1>
+            <pre>{error.status} - {error.statusText}</pre>
+            </>
+        )
+    }
 }
