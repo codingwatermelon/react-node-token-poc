d5bc5e835bbc108fc6d99d6c83a3d3492a5af525
codingwatermelon <32379178+codingwatermelon@users.noreply.github.com>
2024-01-10 03:48:29 -0800

    Update Error.jsx

--- JoeysHomesApp/components/Error.jsx
+++ JoeysHomesApp/components/Error.jsx
@@ -4,28 +4,39 @@ import { Link, useRouteError, isRouteErrorResponse, useLocation } from "react-ro
 export default function Error() {
     const error = useRouteError()
 
+    const defaultError = (
+        <>
+        <h1>Error: {error.message}</h1>
+        <pre>{error.status} - {error.statusText}</pre>
+        </>
+    )
+
     // Handle router errors
     if (isRouteErrorResponse(error)) {
         return (
-            <>
-            <h1>Error: {error.message}</h1>
-            <pre>{error.status} - {error.statusText}</pre>
-            </>
+            {defaultError}
         )
     }
     else {
         // If error is 403 Forbidden, then route user back to Login and mention that their session has timed out
-        if (error.response.status == 403) {
-            const location = `/login?message=Session expired&redirectTo=${useLocation().pathname}`;
-            return (
-                <div className="session-expired">
-                    <h1>Your session has timed out.</h1>
-                    <Link
-                        to={location}
-                    >
-                        <h2>Click here to log in again</h2>
-                    </Link>
-                </div>
+        if ('response' in error) {
+            if (error.response.status == 403) {
+                const location = `/login?message=Session expired&redirectTo=${useLocation().pathname}`;
+                return (
+                    <div className="session-expired">
+                        <h1>Your session has timed out.</h1>
+                        <Link
+                            to={location}
+                        >
+                            <h2>Click here to log in again</h2>
+                        </Link>
+                    </div>
+                )
+            }
+        }
+        else {
+            return(
+                {defaultError}
             )
         }
     }
