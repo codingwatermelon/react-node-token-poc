4ef409793e506d18e9e898f19b8ac952e7178980
codingwatermelon <32379178+codingwatermelon@users.noreply.github.com>
2024-01-17 23:51:02 -0800

    Update Header.jsx

--- JoeysHomesApp/components/Header.jsx
+++ JoeysHomesApp/components/Header.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react"
+import React, { useState, useEffect, useContext } from "react"
 import { Link, NavLink } from "react-router-dom"
 import AuthService from "../services/auth.service";
 
