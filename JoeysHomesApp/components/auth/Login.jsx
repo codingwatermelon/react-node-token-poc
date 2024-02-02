46f809428bc85b4b53cfc0f9d3a9cdb926a865df
codingwatermelon <32379178+codingwatermelon@users.noreply.github.com>
2024-02-01 18:09:51 -0800

    Update Login.jsx

--- JoeysHomesApp/components/auth/Login.jsx
+++ JoeysHomesApp/components/auth/Login.jsx
@@ -98,7 +98,7 @@ export default function Login() {
     const { isAuthenticated, changeHeader } = useContext(AuthContext);
 
     // After the user submits the login form
-    const handleLogin = async (e) => {
+    async function handleLogin (e) {
         e.preventDefault();
         const baseUrl = 'http://192.168.64.3:5173'
         // Perform the login logic, for example, by calling an authentication API
