f357e43e45b5ca82ed957dd8e225289b5282036c
codingwatermelon <32379178+codingwatermelon@users.noreply.github.com>
2024-02-04 02:31:47 -0800

    Update index.jsx

--- JoeysHomesApp/index.jsx
+++ JoeysHomesApp/index.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom/client';
 import {
   RouterProvider,
@@ -30,11 +30,11 @@ import Register from "./components/auth/Register"
 //import "../JoeysHomesServer/server"
 
 // TODO Add usecontext here so that I can pass it into the login action?
-import { AuthContext } from "./components/common/AuthContext"
 
+import { useAuth } from "./components/common/AuthContext"
 
 function App() {
-  const { isAuthenticated, changeHeader } = useContext(AuthContext);
+  const isAuthenticated = useAuth();
 
   const router = createBrowserRouter(createRoutesFromElements(
     <Route path="/" element={<Layout />}>
