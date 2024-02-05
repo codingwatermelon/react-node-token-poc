b9e1167b65f07bba6f08e7b8873ada232b1c9e2a
codingwatermelon <32379178+codingwatermelon@users.noreply.github.com>
2024-02-04 16:35:19 -0800

    Update Login.jsx

--- JoeysHomesApp/components/auth/Login.jsx
+++ JoeysHomesApp/components/auth/Login.jsx
@@ -128,7 +128,7 @@ export default function Login() {
         //    .searchParams.get("redirectTo") || "/"
 
         const pathname = new URLSearchParams(window.location.search)
-            .params.get("redirectTo") || "/"
+            .get("redirectTo") || "/"
         
         if (!(userNameRegex.test(username))) {
             console.log("username is invalid")
