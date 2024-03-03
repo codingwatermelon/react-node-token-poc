a1a71701889d4db3435f47b538be4d373b06d40c
codingwatermelon <32379178+codingwatermelon@users.noreply.github.com>
2024-03-03 03:34:04 -0800

    add data, update SQL commands

--- DB/createTables.sql
+++ DB/createTables.sql
@@ -7,26 +7,45 @@ CREATE TABLE IF NOT EXISTS Properties (
    property_description VARCHAR ( 200 ) NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS PropertiesDetails (
+CREATE TABLE IF NOT EXISTS PropertiesGeneralDetails (
    id int GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
    image_path VARCHAR ( 200 ) NOT NULL,
-   base_value INT NOT NULL,
    purchase_date DATE NOT NULL,
-   CONSTRAINT fk_property_id
+   CONSTRAINT fk_properties_id
        FOREIGN KEY(id) 
 	    REFERENCES Properties(id)
 	    ON DELETE CASCADE
 );
 
+CREATE TABLE IF NOT EXISTS PropertiesFinancialDetails (
+   id int GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+   rental_income INT NOT NULL,
+   net_operating_income INT NOT NULL,
+   cash_flow INT NOT NULL,
+   cash_on_cash_return INT NOT NULL,
+   total_cash_in INT NOT NULL,
+   cap_rate INT NOT NULL,
+   internal_rate_return INT NOT NULL,
+   base_market_value INT NOT NULL,
+   net_present_value INT NOT NULL,
+   loan_to_value_ratio INT NOT NULL,
+   operating_expense_ratio INT NOT NULL,
+   vacancy_rate INT NOT NULL,
+   CONSTRAINT fk_propertiesgeneraldetails_id
+       FOREIGN KEY(id) 
+	    REFERENCES PropertiesGeneralDetails(id)
+	    ON DELETE CASCADE
+);
+
 CREATE TABLE IF NOT EXISTS PropertiesMaintenance (
    id int GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-   property_id INT NOT NULL,
+   properties_id INT NOT NULL,
    maintenance_name VARCHAR ( 200 ) NOT NULL,
    cost INT NOT NULL,
    maintenance_type VARCHAR ( 200 ) NOT NULL,
    due_date DATE NOT NULL,
-   CONSTRAINT fk_property_id
-       FOREIGN KEY(property_id) 
+   CONSTRAINT fk_properties_id
+       FOREIGN KEY(properties_id) 
 	    REFERENCES Properties(id)
 	    ON DELETE CASCADE
 );
--- DB/resetTables.sql
+++ DB/resetTables.sql
@@ -2,4 +2,6 @@ DROP TABLE IF EXISTS propertydetails;
 DROP TABLE IF EXISTS propertiesdetails; 
 DROP TABLE IF EXISTS propertiesmaintenance;
 DROP TABLE IF EXISTS properties;
-DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS propertiesgeneraldetails;
+DROP TABLE IF EXISTS propertiesfinancialdetails;
+--DROP TABLE IF EXISTS users;
