63b58c3d321a601231a22869267b5a497ec30e53
codingwatermelon <32379178+codingwatermelon@users.noreply.github.com>
2024-03-03 17:58:51 -0800

    update sql commands in api

--- JoeysHomesServer/api.js
+++ JoeysHomesServer/api.js
@@ -17,7 +17,7 @@ const getHouses = (houseId) => {
       const id = parseInt(houseId)
       console.log(id)
 
-      pool.query('select properties.id as property_id, property_address, property_description, image_path, base_value, purchase_date from properties join propertiesdetails on properties.id = propertiesdetails.id where properties.id = $1', [id], (error, results) => {
+      pool.query('select properties.id as properties_id, property_address, property_description, image_path, base_value, purchase_date from properties join propertiesgeneraldetails on properties.id = propertiesgeneraldetails.id where properties.id = $1', [id], (error, results) => {
         if (error) {
           reject(error)
         }
@@ -26,7 +26,7 @@ const getHouses = (houseId) => {
       })
     }
     else {
-      pool.query('select properties.id as property_id, property_address, property_description, image_path, base_value, purchase_date from properties join propertiesdetails on properties.id = propertiesdetails.id', (error, results) => {
+      pool.query('select properties.id as properties_id, property_address, property_description, image_path, base_value, purchase_date from properties join propertiesgeneraldetails on properties.id = propertiesgeneraldetails.id', (error, results) => {
         if (error) {
           reject(error)
         }
