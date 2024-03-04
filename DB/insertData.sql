5a375b824fe565a7810f1e0dbf77a9f34d98c6d7
codingwatermelon <32379178+codingwatermelon@users.noreply.github.com>
2024-03-03 17:29:16 -0800

    Update insertData.sql

--- DB/insertData.sql
+++ DB/insertData.sql
@@ -1,19 +1,19 @@
 copy properties(id, property_address, property_description)
-FROM '/home/jack/joeyshomes/DB/data_files/Properties-Table 1.csv'
+FROM '/home/jack/joeyshomes/DB/data_files/Properties.csv'
 DELIMITER ','
 CSV HEADER;
 
 copy propertiesgeneraldetails(image_path, purchase_date)
-FROM '/home/jack/joeyshomes/DB/data_files/PropertiesGeneralDetails-Table 1.csv'
+FROM '/home/jack/joeyshomes/DB/data_files/PropertiesGeneralDetails.csv'
 DELIMITER ','
 CSV HEADER;
 
 copy propertiesfinancialdetails(rental_income, net_operating_income, cash_flow, cash_on_cash_return, total_cash_in, cap_rate, internal_rate_return, base_market_value, net_present_value, loan_to_value_ratio, operating_expense_ratio, vacancy_rate)
-FROM '/home/jack/joeyshomes/DB/data_files/PropertiesFinancialDetails-Table 1.csv' 
+FROM '/home/jack/joeyshomes/DB/data_files/PropertiesFinancialDetails.csv' 
 DELIMITER ',' 
 CSV HEADER;
 
 copy propertiesmaintenance(properties_id, maintenance_name, cost, maintenance_type, due_date)
-FROM '/home/jack/joeyshomes/DB/data_files/PropertiesMaintenance-Table 1.csv'
+FROM '/home/jack/joeyshomes/DB/data_files/PropertiesMaintenance.csv'
 DELIMITER ',' 
 CSV HEADER;
