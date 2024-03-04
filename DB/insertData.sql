8b89ef3717d2e498ef5897349bc061561c732639
codingwatermelon <32379178+codingwatermelon@users.noreply.github.com>
2024-03-03 17:22:08 -0800

    add insert data commands

--- DB/insertData.sql
+++ DB/insertData.sql
@@ -1,4 +1,23 @@
-COPY Properties(property_address, property_description)
-FROM 'data_files/Properties-Table 1.csv'
+copy properties(id, property_address, property_description)
+FROM '/Users/jack/Documents/code/JoeysHomes/joeyshomes/DB/data_files/Properties-Table 1.csv'
 DELIMITER ','
-CSV HEADER;
+CSV HEADER
+QUOTE '\"';
+
+copy propertiesgeneraldetails(image_path, purchase_date)
+FROM '/Users/jack/Documents/code/JoeysHomes/joeyshomes/DB/data_files/PropertiesGeneralDetails-Table 1.csv'
+DELIMITER ','
+CSV HEADER
+QUOTE '\"';
+
+copy propertiesfinancialdetails(rental_income, net_operating_income, cash_flow, cash_on_cash_return, total_cash_in, cap_rate, internal_rate_return, base_market_value, net_present_value, loan_to_value_ratio, operating_expense_ratio, vacancy_rate)
+FROM '/Users/jack/Documents/code/JoeysHomes/joeyshomes/DB/data_files/PropertiesFinancialDetails-Table 1.csv' 
+DELIMITER ',' 
+CSV HEADER
+QUOTE '\"';
+
+copy propertiesmaintenance(properties_id, maintenance_name, cost, maintenance_type, due_date)
+FROM '/Users/jack/Documents/code/JoeysHomes/joeyshomes/DB/data_files/PropertiesMaintenance-Table 1.csv'
+DELIMITER ',' 
+CSV HEADER 
+QUOTE '\"';
--- DB/resetTables.sql
+++ DB/resetTables.sql
@@ -1,7 +1,7 @@
 DROP TABLE IF EXISTS propertydetails; 
 DROP TABLE IF EXISTS propertiesdetails; 
 DROP TABLE IF EXISTS propertiesmaintenance;
-DROP TABLE IF EXISTS properties;
 DROP TABLE IF EXISTS propertiesgeneraldetails;
 DROP TABLE IF EXISTS propertiesfinancialdetails;
+DROP TABLE IF EXISTS properties;
 --DROP TABLE IF EXISTS users;
