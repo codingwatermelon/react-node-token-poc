591c2071e7021a05c1f9f2f4ae0364720b5b096a
codingwatermelon <32379178+codingwatermelon@users.noreply.github.com>
2024-03-08 14:47:43 -0800

    Update createTables.sql

--- DB/createTables.sql
+++ DB/createTables.sql
@@ -8,10 +8,10 @@ CREATE TABLE IF NOT EXISTS PropertiesBasicGeneralDetails (
    id int PRIMARY KEY,
    image_path VARCHAR ( 200 ) NOT NULL,
    purchase_date DATE NOT NULL,
-   rental_income INT NOT NULL,
-   operating_expenses INT NOT NULL,
-   purchase_price INT NOT NULL,
-   vacancy_rate INT NOT NULL,
+   rental_income NUMERIC(10,2) NOT NULL,
+   operating_expenses NUMERIC(10,2) NOT NULL,
+   purchase_price NUMERIC(10,2) NOT NULL,
+   vacancy_rate NUMERIC(10,2) NOT NULL,
    CONSTRAINT fk_properties_id
        FOREIGN KEY(id) 
 	    REFERENCES Properties(id)
@@ -20,13 +20,13 @@ CREATE TABLE IF NOT EXISTS PropertiesBasicGeneralDetails (
 
 CREATE TABLE IF NOT EXISTS PropertiesDerivedFinancialDetails (
    id int PRIMARY KEY,
-   cash_flow INT NOT NULL,
-   cash_on_cash_return INT NOT NULL,
-   total_cash_in INT NOT NULL,
-   cap_rate INT NOT NULL,
-   net_present_value INT NOT NULL,
-   loan_to_value_ratio INT NOT NULL,
-   operating_expense_ratio INT NOT NULL,
+   cash_flow NUMERIC(10,2) NOT NULL,
+   cash_on_cash_return NUMERIC(10,2) NOT NULL,
+   total_cash_in NUMERIC(10,2) NOT NULL,
+   cap_rate NUMERIC(10,2) NOT NULL,
+   net_present_value NUMERIC(10,2) NOT NULL,
+   loan_to_value_ratio NUMERIC(10,2) NOT NULL,
+   operating_expense_ratio NUMERIC(10,2) NOT NULL,
    CONSTRAINT fk_properties_id
        FOREIGN KEY(id) 
 	    REFERENCES Properties(id)
@@ -37,7 +37,7 @@ CREATE TABLE IF NOT EXISTS PropertiesMaintenance (
    id int GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
    properties_id INT NOT NULL,
    maintenance_name VARCHAR ( 200 ) NOT NULL,
-   cost INT NOT NULL,
+   cost NUMERIC(10,2) NOT NULL,
    maintenance_type VARCHAR ( 200 ) NOT NULL,
    due_date DATE NOT NULL,
    CONSTRAINT fk_properties_id
