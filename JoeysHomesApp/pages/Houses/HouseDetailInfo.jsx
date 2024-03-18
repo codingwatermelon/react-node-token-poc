8a389e1b8b64e1e73d5d716e59597e04d877ee4e
codingwatermelon <32379178+codingwatermelon@users.noreply.github.com>
2024-03-18 16:21:57 -0700

    Update HouseDetailInfo.jsx

--- JoeysHomesApp/pages/Houses/HouseDetailInfo.jsx
+++ JoeysHomesApp/pages/Houses/HouseDetailInfo.jsx
@@ -17,7 +17,14 @@ export default function HouseDetailInfo() {
     return (
         <div>
             <h3>Test</h3>
-            
+            <div className="house-detail">
+                <p>Purchased on {new Date(currentHouse.purchase_date).toDateString()} for {currencyFormat(currentHouse.purchase_price)}</p>
+                <p>Vacancy Rate: {currentHouse.vacancy_rate}%</p>
+                <p>Rental Income: {currencyFormat(currentHouse.rental_income)}</p>
+                <p>Operating Expenses: {currencyFormat(currentHouse.operating_expenses)}</p>
+                <p>Operating Expense Ratio: {currentHouse.operating_expense_ratio}</p>
+            </div>
+
         </div>
     )
 }
