7cbdfd3913b41d5a61b3b1d6cf0cc0309faf6cb6
codingwatermelon <32379178+codingwatermelon@users.noreply.github.com>
2024-03-18 16:34:20 -0700

    Update HouseDetailInfo.jsx

--- JoeysHomesApp/pages/Houses/HouseDetailInfo.jsx
+++ JoeysHomesApp/pages/Houses/HouseDetailInfo.jsx
@@ -6,38 +6,45 @@ import {
 export default function HouseDetailInfo() {
     const { dataPromise } = useOutletContext()
 
-    const currentHouse = dataPromise.house
+    function renderHouseElements(house) {
 
-    console.log(dataPromise)
-    console.log(currentHouse)
+        function currencyFormat(num) {
+            let dollar = new Intl.NumberFormat('en-US', {
+                style: 'currency',
+                currency: 'USD'
+            });
+            return dollar.format(num);
+        }
 
-    const houseElements = currentHouse.map(house => (
-        <div key={house.properties_id}>
-            <div className="house-info">
-                <p>Purchased on {new Date(currentHouse.purchase_date).toDateString()} for {currencyFormat(currentHouse.purchase_price)}</p>
-                <p>Vacancy Rate: {currentHouse.vacancy_rate}%</p>
-                <p>Rental Income: {currencyFormat(currentHouse.rental_income)}</p>
-                <p>Operating Expenses: {currencyFormat(currentHouse.operating_expenses)}</p>
-                <p>Operating Expense Ratio: {currentHouse.operating_expense_ratio}</p>
+        const displayedHouses = house
+
+        const houseElements = displayedHouses.map(house => (
+            <div key={house.properties_id}>
+                <div className="house-info">
+                    <p>Purchased on {new Date(house.purchase_date).toDateString()} for {currencyFormat(house.purchase_price)}</p>
+                    <p>Vacancy Rate: {house.vacancy_rate}%</p>
+                    <p>Rental Income: {currencyFormat(house.rental_income)}</p>
+                    <p>Operating Expenses: {currencyFormat(house.operating_expenses)}</p>
+                    <p>Operating Expense Ratio: {house.operating_expense_ratio}</p>
+                </div>
             </div>
-        </div>
-    ))
-
-    function currencyFormat(num) {
-        let dollar = new Intl.NumberFormat('en-US', {
-            style: 'currency',
-            currency: 'USD'
-        });
-        return dollar.format(num);
+        ))
+    
+        return (
+            <div className="house-detail-container">
+                {houseElements}
+            </div>
+        )
     }
 
     return (
-        <div>
-            <h3>Test</h3>
-            <div className="house-detail">
-                {houseElements}
-            </div>
-
+        <div className="house-list-container">
+            <React.Suspense fallback={<h2>Loading house...</h2>}>
+                <Await resolve={dataPromise.house}>
+                    {renderHouseElements}
+                </Await>
+            </React.Suspense>
         </div>
+        
     )
 }
