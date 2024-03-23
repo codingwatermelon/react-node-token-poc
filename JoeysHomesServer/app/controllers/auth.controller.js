6d21645a6ba4efa5b91eae269f3f51798752d133
codingwatermelon <32379178+codingwatermelon@users.noreply.github.com>
2024-03-22 23:57:37 -0700

    Update auth.controller.js

--- JoeysHomesServer/app/controllers/auth.controller.js
+++ JoeysHomesServer/app/controllers/auth.controller.js
@@ -1,4 +1,5 @@
-import { MailtrapClient } from "mailtrap"
+//import { MailtrapClient } from "mailtrap"
+const mailtrap = require("mailtrap")
 require('dotenv').config()
 
 const db = require("../models");
@@ -119,7 +120,7 @@ exports.submitPasswordReset = (req, res) => {
       const SENDER_EMAIL = process.env.SENDER_EMAIL || ''
       const BASE_URL = "http://192.168.64.3:5173"
 
-      const client = new MailtrapClient({ token: MAILTRAP_TOKEN });
+      const client = mailtrap.MailtrapClient({ token: MAILTRAP_TOKEN });
 
       const sender = { name: "JoeysHomes", email: SENDER_EMAIL };
       
